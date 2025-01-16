3b7e11f5648f140258317baf984d46f68f776a3c
rebecaverasa <rebecaverasa@gmail.com>
2025-01-16 12:54:10 -0300

    tasks style and logic for toggle completed

--- src/components/Tasks.tsx
+++ src/components/Tasks.tsx
@@ -1,20 +1,50 @@
-import styles from './Tasks.module.css'
+import { FaTrashAlt } from 'react-icons/fa';
+import styles from './Tasks.module.css';
+import { useState } from 'react';
 
 export function Tasks() {
+    const [tasks, setTasks] = useState([
+        {id: 1, text: 'Exemplo de tarefa pendente', completed: false},
+        {id: 2, text: 'Exemplo de tarefa concluida', completed: true}
+    ]);
+
+    const toggleTaskComplete = (id: number) => {
+        setTasks((prevTasks) =>
+            prevTasks.map((task) =>
+                task.id === id
+                    ? { ...task, completed: !task.completed }
+                    : task
+            )
+        );
+    };
+
     return (
         <div className={styles.tasksContainer}>
-            <span className={styles.task}>blablablabla</span>
-            <span className={styles.task}>blablabla</span>
-            <span className={styles.task}>blablabla</span>
-            <span className={styles.task}>blablabla</span>
-            <span className={styles.task}>blablabla</span>
-            <span className={styles.task}>blablabla</span>
-            <span className={styles.task}>blablabla</span>
-            <span className={styles.task}>blablabla</span>
-            <span className={styles.task}>blablabla</span>
-            <span className={styles.task}>blablabla</span>
-            <span className={styles.task}>blablabla</span>
-            <span className={styles.task}>blablabla</span>
+            {tasks.map((task) => (
+                <div
+                    key={task.id}
+                    className={`${styles.task} ${
+                        task.completed ? styles.completed : ''
+                    }`}
+                >
+                    <div 
+                        className={styles.taskContent}
+                        onClick={() => toggleTaskComplete(task.id)}
+                    >
+                        <div
+                            className={
+                                task.completed
+                                    ? styles.iconCompleted
+                                    : styles.iconPending
+                            }
+                        />
+                        <span>{task.text}</span>
+                    </div>
+                    <button className={styles.deleteButton}>
+                        <FaTrashAlt />
+                    </button>
+                </div>
+            ))}
         </div>
-    )
+    );
 }
